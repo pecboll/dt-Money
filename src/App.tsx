11aa43635254f42378a52d2ccd25b33d4f6fec21
Igor Rocha <pecboll@gmail.com>
2021-03-15 12:18:29 -0300

    inicial push
    algumas modificaçoes iniciais

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Header } from "./components/Header";
+import { GlobalStyles } from "./styles/global";
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <GlobalStyles />
+    </>
+  );
+}
+
